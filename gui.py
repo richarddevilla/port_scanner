53440ee49841a10fe2caed9f070f9be4d76d4f0f
richarddevilla <chaseyba@gmail.com>
2018-05-18 14:18:16 +1000

    completed functions

--- gui.py
+++ gui.py
@@ -2,6 +2,7 @@ from tkinter import ttk, Tk, StringVar, messagebox
 from socket import *
 from threading import *
 
+#Dictionary of common ports
 ports = {
             1:{'port': 20,'name': 'File Transfer Protocol (FTP)'},
             2:{'port': 21,'name': 'File Transfer Protocol (FTP)'},
@@ -24,7 +25,9 @@ ports = {
             19:{'port': 443,'name': 'HTTP with Secure Sockets Layer (SSL)'}
 }
 
+
 def main():
+    #initializes the widgets of Tkinter
     root = Tk()
     host_var = StringVar()
     port_var = StringVar()
@@ -39,12 +42,24 @@ def main():
     result_tree.heading('Port Name', text='Port Name')
     result_tree.heading('Result', text='Result')
     result_tree['show'] = 'headings'
+
     def scan_conn(host,port,name='NA'):
+        """
+
+        :param host: hostname or ip address of target as string
+        :param port: port number as int
+        :param name: port name as string, defaults to 'NA' if not given
+
+        create a socket connection to port and host.
+        try to send a garbage data if it succeed set port_status as 'Open'
+        if it fails set port_status as 'Close'
+        finally insert port, name and port_status on the result_tree
+
+        """
         try:
             socket_conn = socket(AF_INET, SOCK_STREAM)
             socket_conn.connect((host,port))
-            socket_conn.send('TestTestTest')
-            result = socket_conn.recv(100)
+            socket_conn.send(b'test\r\n')
             port_status = 'Open'
         except:
             port_status = 'Close'
@@ -53,10 +68,19 @@ def main():
             socket_conn.close()
 
     def port_scan(host,ports):
+        """
+
+        :param host: hostname or ip address of target as string
+        :param ports: ports as string
+
+        test host if valid
+        split ports by comma and put result as list
+        then start a thread and run scan_conn per port in ports
+        """
         try:
-            host_ip = gethostbyname(host)
+            gethostbyname(host)
         except:
-            messagebox.showerror('Unknown Host!')
+            messagebox.showerror('Unknown Host!','Please enter a valid hostname or ip!')
             return
         ports = ports.split(',')
         for port in ports:
@@ -64,23 +88,35 @@ def main():
             t.start()
 
     def port_scan_all(host):
+        """
+
+        :param host: hostname or ip address of target as string
+
+        test host if valid
+        then start a thread and run scan_conn per port of the ports dictionary
+        """
         try:
-            host_ip = gethostbyname(host)
+            gethostbyname(host)
         except:
-            messagebox.showerror('Unknown Host!')
+            messagebox.showerror('Unknown Host!','Please enter a valid hostname or ip!')
             return
         for port,values in ports.items():
             t = Thread(target=scan_conn, args=(host,values['port'],values['name']))
             t.start()
 
     def clear():
+        """
+        clear the values from result_tree, host entry field and port entry field
+        """
         result_tree.delete(*result_tree.get_children())
         host_var.set('')
         port_var.set('')
 
+    #initializes the buttons of the GUI
     scan_btn = ttk.Button(root,text='Scan',command=lambda :port_scan(host_var.get(),port_var.get()))
     scan_all_btn = ttk.Button(root, text='Scan All Common Ports', command=lambda :port_scan_all(host_var.get()))
     clear_btn = ttk.Button(root, text='Clear',command=lambda :clear())
+    #geometry manager settings
     host_label.pack()
     host_entry.pack()
     port_label.pack()
@@ -89,15 +125,12 @@ def main():
     scan_btn.pack()
     clear_btn.pack()
     scan_all_btn.pack()
-    root.mainloop()
+    return root
 
 
-
-
-
-
-
-main()
+if __name__ == '__main__':
+    main = main()
+    main.mainloop()
 
 
 
