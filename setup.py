46051ace4173c2cd77a2729a49ad8aa9e751b3ee
richarddevilla <chaseyba@gmail.com>
2018-05-18 15:14:53 +1000

    completed functions

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
-from setuptools import setup
+import cx_Freeze
+import sys
 
-setup(
-    name='port_scanner',
-    version='',
-    packages=[''],
-    url='',
-    license='',
-    author='Chase',
-    author_email='',
-    description=''
+base = None
+
+if sys.platform == 'win32':
+    base = 'Win32GUI'
+
+executables = [cx_Freeze.Executable("port_scanner.py",base=base)]
+
+cx_Freeze.setup(
+    name = 'port_scanner',
+    options = {'build_exe': {'packages':['tkinter','socket','threading']}},
+    version = '1.0',
+    description = 'Scan open ports',
+    executables = executables
 )
